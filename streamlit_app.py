9ee68d2d7ef49547ee4683f48b631981d0fe4317
RajeshMuniswamy <133192832+RajeshMuniswamy@users.noreply.github.com>
2023-05-11 11:47:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 
 streamlit.title('Breakfast favorites')
-streamlit.header('Breakfast Menu')
+# streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
