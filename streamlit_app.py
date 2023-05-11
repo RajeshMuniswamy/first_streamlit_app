f12b194aa78139c0a77512d9904e1543fd4170b7
RajeshMuniswamy <133192832+RajeshMuniswamy@users.noreply.github.com>
2023-05-11 11:37:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+# streamlit.title('My Parents New Healthy Diner')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
